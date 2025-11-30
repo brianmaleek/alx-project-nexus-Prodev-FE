4b67748e774a9a408487964955da24c794f5e285
Brian Maleek <maleekmalik@gmail.com>
2025-11-30 18:06:05 +0300

    feat: implement main Index component with movie fetching and display

---
+++ app/(appPage)/index.tsx
@@ -0,0 +1,104 @@
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import {ActivityIndicator, Image, ScrollView, View, Text, FlatList} from "react-native";
+import {useRouter} from "expo-router";
+import useFetch from "@/servicesApi/useFetch";
+import {fetchMovies} from "@/servicesApi/api";
+import MovieCard from "@/components/movieCard";
+import TrendingCard from "@/components/TrendingCard";
+import {getTrendingMovies} from "@/servicesApi/appwrite";
+
+export default function Index() {
+  const router = useRouter();
+
+  const {
+	  data: trendingMovies,
+	  loading: trendingMoviesLoading,
+	  error: trendingMoviesError} = useFetch(getTrendingMovies);
+
+  const {
+      data: movies,
+      loading: moviesLoading,
+      error: moviesError} = useFetch(() => fetchMovies( { query: ''}))
+
+  return (
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+
+      <ScrollView className="flex-1 px-5"
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
+        <Image source={icons.logo} className="w-12 h-10 mb-5 mt-20 mx-auto" />
+          { moviesLoading || trendingMoviesLoading ? (
+              <ActivityIndicator
+                  size="large"
+                  color="#0000ff"
+                  className="mt-10 self-center"
+              />
+          ) : moviesError || trendingMoviesError ? (
+              <Text>Error: {moviesError?.message || trendingMoviesError?.message}</Text>
+          ) : (
+              <View className="flex-1 mt-5">
+                  <SearchBar onPress={() => router.push("/search")}
+                             placeholder="Search movies, TV shows..."
+                             value={""}
+                             onChangeText={function (text: string): void {
+                                 throw new Error("Function not implemented.");
+                             }}>
+                  </SearchBar>
+
+	              {trendingMovies && (
+					  <View className={"mt-10"}>
+						  <Text className={"text-lg text-white mb-3 font-bold"}>
+							  Trending Movies
+						  </Text>
+
+						  <FlatList
+							  horizontal
+							  showsHorizontalScrollIndicator={false}
+							  className="mb-4 mt-3"
+							  data={trendingMovies}
+							  contentContainerStyle={{
+								  gap: 26,
+							  }}
+							  renderItem={({ item, index }) => (
+								  <TrendingCard movie={item} index={index}/>
+							  )}
+							  keyExtractor={(item) => item.movie_id.toString()}
+							  ItemSeparatorComponent={() => <View className="w-4" />}
+						  />
+					  </View>
+	              )}
+
+                  <>
+                      <Text className=
+                                "text-lg text-white font-bold mt-5 mb-3">
+                          Latest Movies
+                      </Text>
+                      
+                      <FlatList
+                          data={movies}
+                          renderItem={({ item }) => (
+                              <MovieCard
+                                  { ...item }
+                              />
+                          )}
+                          keyExtractor={( item ) => item.id.toString()}
+                          numColumns={3}
+                          columnWrapperStyle={{
+                              justifyContent: 'flex-start',
+                              gap: 20,
+                              paddingRight: 5,
+                              marginBottom: 10
+                          }}
+                          className="mt-2 pb-32"
+                          scrollEnabled={false}
+                      />
+                  </>
+              </View>
+          )}
+      </ScrollView>
+    </View>
+  );
+}
